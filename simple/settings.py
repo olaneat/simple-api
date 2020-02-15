a20e0313381c00d70eee4a4aeab53b2282c1aa7d
Tosin <tosinayoola0@gmail.com>
2020-02-15 03:18:21 +0100

    changes

--- simple/settings.py
+++ simple/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 )
 STATIC_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
