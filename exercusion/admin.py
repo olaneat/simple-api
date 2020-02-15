3f3cc9518d9f3612b867e229eee6915e7d67ad1a
Tosin <tosinayoola0@gmail.com>
2020-02-15 01:00:11 +0100

    miggrations

--- exercusion/admin.py
+++ exercusion/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Execursion
 # Register your models here.
 
+@admin.register(Execursion)
 class ExecursionAdmin(admin.ModelAdmin):
     list_display = ['name']
