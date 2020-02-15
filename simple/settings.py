101f4557317d8aace247f272a1b2fd517ef1f984
Tosin <tosinayoola0@gmail.com>
2020-02-15 03:24:52 +0100

    changes

--- simple/settings.py
+++ simple/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,8 +130,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static'),
 )
-STATIC_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django.heroku.settings(locals())
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
