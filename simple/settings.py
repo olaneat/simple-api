59d611c2c57e02d4b46ed4f7b7a9ecf4a675805d
Tosin <tosinayoola0@gmail.com>
2020-02-15 03:29:09 +0100

    changes

--- simple/settings.py
+++ simple/settings.py
@@ -133,6 +133,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django.heroku.settings(locals())
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
